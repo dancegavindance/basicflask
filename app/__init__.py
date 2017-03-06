76e76e5ad55820f446c4d1b677fa40a0cf032a8c
ErikNestor <erik.nestor@insuremytrip.com>
2017-03-06 14:56:29 -0500

    The official bringing of the funk

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask, Blueprint
 from app.api_numpy.views import numpy
 from app.api_intertools.views import intertools
+from app.api_funky.views import funky
+
 APP = Flask(__name__)
 APP.register_blueprint(numpy)
 APP.register_blueprint(intertools)
+APP.register_blueprint(funky)
+
+
+
+
